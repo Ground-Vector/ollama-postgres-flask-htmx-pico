315366ebf5b0ede89c428cfa9f4de69ba715eb20
Marek Michalik <marekmichalik43@gmail.com>
2026-03-16 16:05:44 +0100

    Add Ollama

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import os
+
 from flask import Flask, render_template, request
+from ollama import Client
 
 from database import get_prompts, add_user, get_prompts_without_response, add_response
 from library import database_handle
@@ -27,6 +30,11 @@ def register():
 
 @app.route("/cron")
 def cron():
+    client = Client(
+        host=os.environ.get("OLLAMA_HOST"),
+        headers={"Authorization": "Bearer " + os.environ.get("OLLAMA_AUTH")},
+    )
     for prompt in get_prompts_without_response(db):
-        add_response(db, prompt["id"], "test2")
+        response = client.chat(model=os.environ.get("OLLAMA_MODELS").split(",")[0], messages=[{"role": "user", "content": prompt["prompt"]}])
+        add_response(db, prompt["id"], response["message"]["content"].strip())
     return "OK"
